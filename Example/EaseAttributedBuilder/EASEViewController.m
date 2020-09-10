71e2677aa806ca6715a02a6ffd2f6979f960a04f
Y_rocky <yangheng@ibobei.cn>
2020-09-10 11:44:26 +0800

    modif:为README编写使用案例

--- Example/EaseAttributedBuilder/EASEViewController.m
+++ Example/EaseAttributedBuilder/EASEViewController.m
@@ -11,6 +11,7 @@
 
 @interface EASEViewController ()
 
+@property (nonatomic ,strong) UIStackView * contentView;
 @end
 
 @implementation EASEViewController
@@ -19,18 +20,100 @@
 {
     [super viewDidLoad];
     
+    self.contentView = [UIStackView new];
+    self.contentView.frame = self.view.bounds;
+    self.contentView.spacing = 10;
+    self.contentView.alignment = UIStackViewAlignmentCenter;
+    self.contentView.axis = UILayoutConstraintAxisVertical;
+    self.contentView.distribution = UIStackViewDistributionFillEqually;
+    [self.view addSubview:self.contentView];
+    
+    [self.contentView addArrangedSubview:[self example_1]];
+    [self.contentView addArrangedSubview:[self example_2]];
+    [self.contentView addArrangedSubview:[self example_3]];
+    [self.contentView addArrangedSubview:[self example_4]];
+    [self.contentView addArrangedSubview:[self example_5]];
+}
+
+- (UIView *) example_1{
+    
     UILabel * textLabel = [UILabel new];
     textLabel.attributedText =
     [[[[EaseAttributedBuilder builderWithDefaultStyle:@{
         NSForegroundColorAttributeName: UIColor.redColor,
         NSFontAttributeName: [UIFont systemFontOfSize:17 weight:UIFontWeightLight]
     }] appendString:@"Hello World~"]
-     appendString:@"make it Ease!" forStyle:@{
+     appendString:@" make it Ease!" forStyle:@{
         NSForegroundColorAttributeName: UIColor.orangeColor,
      }] attributedString];
     [textLabel sizeToFit];
-    textLabel.center = self.view.center;
-    [self.view addSubview:textLabel];
+    return textLabel;
+}
+
+- (UIView *) example_2{
+    
+    UILabel * textLabel = [UILabel new];
+    textLabel.attributedText =
+    [[[[[EaseAttributedBuilder builderWithDefaultStyle:@{
+        NSForegroundColorAttributeName: UIColor.orangeColor,
+        NSFontAttributeName: [UIFont systemFontOfSize:17 weight:UIFontWeightLight]
+    }] appendString:@"Hello "]
+     appendString:@"World" forStyle:@{
+        NSForegroundColorAttributeName: UIColor.greenColor,
+     }] appendString:@"~"]
+     attributedString];
+    [textLabel sizeToFit];
+    return textLabel;
+}
+
+- (UIView *) example_3{
+    
+    UILabel * textLabel = [UILabel new];
+    textLabel.attributedText =
+    [[[[[EaseAttributedBuilder builderWithDefaultStyle:@{
+        NSForegroundColorAttributeName: UIColor.orangeColor,
+        NSFontAttributeName: [UIFont systemFontOfSize:17 weight:UIFontWeightLight]
+    }] appendString:@"Hello "]
+     appendString:@"World" forStyle:@{
+        NSForegroundColorAttributeName: UIColor.greenColor,
+     }] appendAttributedString:({
+        [[NSAttributedString alloc] initWithString:@" 你好" attributes:@{
+            NSFontAttributeName:[UIFont systemFontOfSize:34],
+            NSForegroundColorAttributeName:UIColor.redColor
+        }];
+    })] attributedString];
+    [textLabel sizeToFit];
+    return textLabel;
+}
+
+- (UIView *) example_4{
+    
+    UILabel * textLabel = [UILabel new];
+    textLabel.attributedText =
+    [[[EaseAttributedBuilder builderWithString:@"a[bc]d(you)A[BCD]1【大家好】2a[gs]34(me)"]
+        configString:@"(?<=\\[)[^\\]]+" withStyle:@{
+            NSForegroundColorAttributeName: UIColor.greenColor
+        }] attributedString];
+    [textLabel sizeToFit];
+    return textLabel;
+}
+
+- (UIView *) example_5{
+    
+    UILabel * textLabel = [UILabel new];
+    textLabel.attributedText =
+    [[[[[EaseAttributedBuilder builderWithString:@"hello = nihao = Hello = 你好 = nihao"]
+        configString:@"hello" withStyle:@{
+            NSUnderlineColorAttributeName: [UIColor redColor],
+            NSUnderlineStyleAttributeName: @1
+        }] configString:@"nihao" withStyle:@{
+            NSStrokeColorAttributeName: [UIColor orangeColor],
+            NSStrokeWidthAttributeName: @1
+        }] configString:@"H" withStyle:@{
+            NSBackgroundColorAttributeName:[UIColor greenColor]
+        }] attributedString];
+    [textLabel sizeToFit];
+    return textLabel;
 }
 
 - (void)didReceiveMemoryWarning
--- README.md
+++ README.md
@@ -9,6 +9,84 @@
 
 To run the example project, clone the repo, and run `pod install` from the Example directory first.
 
+EaseAttributedBuilder是为了便捷的创建属性字符串，对NSAttributedString的一个封装，保留了UIKit设置样式的方法，使用起来没有很大的陌生感。
+
+```Objective-C
+
+_innerBuilder = [EaseAttributedBuilder builderWithDefaultStyle:@{
+    NSFontAttributeName: [UIFont systemFontOfSize:15],
+    NSForegroundColorAttributeName: [UIColor orangeColor],
+    NSParagraphStyleAttributeName: ({
+        NSMutableParagraphStyle * style = [NSMutableParagraphStyle new];
+        style.lineSpacing = 2;
+        style;
+    })
+}];
+```
+接着就可以追加文本了，可以使用初始化的样式，也可以为文本设置单独的样式：
+
+``` Objective-C
+
+/// 追加的文本使用初始化的样式
+[_innerBuilder appendString:text];
+
+/// 为追加的文本设置特殊的样式
+[_innerBuilder appendString:text forStyle:@{
+    NSForegroundColorAttributeName: UIColor.greenColor
+}];
+```
+单独的样式并不影响之后追加的文本：
+
+``` Objective-C
+
+_innerBuilder = [EaseAttributedBuilder builderWithDefaultStyle:@{
+    NSFontAttributeName: [UIFont systemFontOfSize:15],
+    NSForegroundColorAttributeName: [UIColor orangeColor]
+}];
+
+/// Hello is orange
+[_innerBuilder appendString:@"Hello "];
+
+/// Wrold is green
+[_innerBuilder appendString:@"Wrold" forStyle:@{
+    NSForegroundColorAttributeName: UIColor.greenColor
+}];
+
+/// ~ is also orange
+[_innerBuilder appendString:@"~"];
+```
+
+除了可以追加文本，还可以追加`NSAttributedString`和`NSTextAttachment`：
+``` Objective-C
+
+[_innerBuilder appendAttributedString:({
+    [[NSAttributedString alloc] initWithString:otherText attributes:@{
+        NSFontAttributeName:[UIFont systemFontOfSize:24],
+        NSForegroundColorAttributeName:UIColor.redColor
+    }];
+})];
+
+[_innerBuilder appendAttachment:({
+    NSTextAttachment *imageAtt = [[NSTextAttachment alloc] init];
+    imageAtt.bounds = CGRectMake(0, 0, 18, 16);
+    imageAtt.image = [UIImage imageNamed:@""];
+    imageAtt;
+})];
+```
+
+如果需要给一个给定的文本中某些字符串设定特殊的样式，可以使用一下api，参数`string`支持正则表达式，内部其实也是使用正则表达式来完成查找匹配的。
+
+``` Objective-C
+
+/// 第一次匹配的文本
+- (EaseAttributedBuilder *) firstMatchConfigString:(NSString *)string
+                                         withStyle:(EaseAttributesStyle)style;
+
+/// 所有匹配的文本
+- (EaseAttributedBuilder *) configString:(NSString *)string 
+                               withStyle:(EaseAttributesStyle)style;
+```
+
 ## Requirements
 
 ## Installation
